19d2a5f7ef0941bb45221d72d3f26c5d5e29a474
Mefrid <oficialbag@gmail.com>
2026-03-03 20:14:51 +0500

    initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,53 @@
+import js from "@eslint/js"
+import { defineConfig, globalIgnores } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import importPlugin from "eslint-plugin-import"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+import viteConfig from "./vite.config.ts"
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      "import/resolver": {
+        // You will also need to install and configure the TypeScript resolver
+        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
+        typescript: true,
+        node: true,
+        vite: {
+          viteConfig, // named export of the Vite config object.
+        },
+      },
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+    },
+  },
+])
